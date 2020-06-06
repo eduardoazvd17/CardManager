5af298f81e8d941ab495eb22406f26f5d30df7a9
Eduardo Azevedo <eduardoazevedo@MacBook-Pro-de-Eduardo.local>
2020-06-06 09:53:18 -0300

    Impementação de metodos toJson e fromJson

--- lib/modelos/cartao.dart
+++ lib/modelos/cartao.dart
@@ -15,4 +15,28 @@ class Cartao {
     this.titular,
     this.numero,
   });
+
+  Cartao.fromJson(Map cartaoMap) {
+    this.numero = cartaoMap['numero'];
+    this.titular = cartaoMap['titular'];
+    this.codigoSeguranca = cartaoMap['codigoSeguranca'];
+    this.mesVencimento = cartaoMap['mesVencimento'];
+    this.anoVencimento = cartaoMap['anoVencimento'];
+    this.senha4 = cartaoMap['senha4'];
+    this.senha6 = cartaoMap['senha6'];
+    this.senha8 = cartaoMap['senha8'];
+  }
+
+  toJson() {
+    Map<String, dynamic> cartaoMap = {};
+    cartaoMap.putIfAbsent('numero', () => numero);
+    cartaoMap.putIfAbsent('titular', () => titular);
+    cartaoMap.putIfAbsent('codigoSeguranca', () => codigoSeguranca);
+    cartaoMap.putIfAbsent('mesVencimento', () => mesVencimento);
+    cartaoMap.putIfAbsent('anoVencimento', () => anoVencimento);
+    cartaoMap.putIfAbsent('senha4', () => senha4);
+    cartaoMap.putIfAbsent('senha6', () => senha6);
+    cartaoMap.putIfAbsent('senha8', () => senha8);
+    return cartaoMap;
+  }
 }
--- lib/modelos/usuario.dart
+++ lib/modelos/usuario.dart
@@ -10,4 +10,34 @@ class Usuario {
     this.senha,
     this.cartoes,
   });
+
+  adicionarCartao(Cartao c) {
+    this.cartoes.add(c);
+  }
+
+  removerCartao(Cartao c) {
+    cartoes.removeWhere((cartao) => cartao.numero == c.numero);
+  }
+
+  Usuario.fromJson(Map usuarioMap) {
+    this.nome = usuarioMap['nome'];
+    this.senha = usuarioMap['senha'];
+    List<Cartao> listaCartoes = [];
+    for (var cMap in usuarioMap['cartoes']) {
+      listaCartoes.add(Cartao.fromJson(cMap));
+    }
+    this.cartoes = listaCartoes;
+  }
+
+  toJson() {
+    Map<String, dynamic> usuarioMap = {};
+    usuarioMap.putIfAbsent('nome', () => nome);
+    usuarioMap.putIfAbsent('senha', () => senha);
+    List<Map<String, dynamic>> cartoesMap = [];
+    for (var c in cartoes) {
+      cartoesMap.add(c.toJson());
+    }
+    usuarioMap.putIfAbsent('cartoes', () => cartoesMap);
+    return usuarioMap;
+  }
 }
